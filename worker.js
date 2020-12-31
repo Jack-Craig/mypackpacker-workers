a500ab9e1118ab92660ba16ee448b7496bc85597
jackcraig <jvcraig2@illinois.edu>
2020-12-30 20:50:19 -0700

    added redis connection

--- worker.js
+++ worker.js
@@ -1,1 +1,40 @@
-console.log("I am a worker! Fuck me!")
+const RedisSMQ = require('rsmq')
+
+const QUEUENAME = 'testqueue'
+const NAMESPACE = 'rsmq'
+
+const REDIS_HOST = process.env.REDIS_HOST
+const REDIS_PORT = process.env.REDIS_PORT
+const REDIS_PASS = process.env.REDIS_PASS
+
+let rsmq = new RedisSMQ({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    ns: NAMESPACE,
+    password: REDIS_PASS
+})
+
+const start = () => {
+    console.log('Worker Initiated!')
+    setInterval(() => {
+        rsmq.receiveMessage({qname: QUEUENAME}, (err, res) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            if (res.id) {
+                console.log(`Received message ${res.id}`)
+                rsmq.deleteMessage({qname: QUEUENAME, id: res.id}, e => {
+                    if (e) {
+                        console.error(e)
+                        return
+                    }
+                    console.log('Deleted message')
+                })
+            } {
+                console.log('No message in queue')
+            }
+        })
+    }, 2000)
+}
+start()
