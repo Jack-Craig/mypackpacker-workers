b66c268b6102bf4647090d15e7698fd0cc444a7a
jackcraig <jvcraig2@illinois.edu>
2020-12-31 20:01:14 -0700

    added senderId

--- subprocessess/adminMessages.js
+++ subprocessess/adminMessages.js
@@ -1,5 +1,6 @@
 const MessageModel = require('../models/Message')
-const handleMessage = (messageObj) => new Promise((resolve, reject) => [
+const handleMessage = (messageObj) => new Promise((resolve, reject) => {
+    console.log(messageObj)
     MessageModel.create(messageObj).then(resolve).catch(reject)
-])
+})
 module.exports = handleMessage
