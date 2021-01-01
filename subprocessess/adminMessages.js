d9ff89881c6a5e68f313e4b5c40566d36b874fd5
jackcraig <jvcraig2@illinois.edu>
2020-12-31 19:08:28 -0700

    fixed typo

---
+++ subprocessess/adminMessages.js
@@ -0,0 +1,5 @@
+const MessageModel = require('../models/Message')
+const handleMessage = (messageObj) => new Promise((resolve, reject) => [
+    MessageModel.create(messageObj).then(resolve).catch(reject)
+])
+module.exports = handleMessage
