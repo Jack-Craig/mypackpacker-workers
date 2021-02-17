f8fbe2f595bfc36b7edc15128b23628fb5fa20e0
unknown <jackdelcraig@gmail.com>
2021-02-17 02:21:59 -0600

    added session model

--- subprocessess/analyticsCompiler.js
+++ subprocessess/analyticsCompiler.js
@@ -2,13 +2,14 @@ const UserModel = require('../models/user')
 const GearModel = require('../models/Product')
 const PackModel = require('../models/Build')
 const AnalyticsModel = require('../models/AnalyticsEntry')
+const SessionModel = require('../models/Session')
 
 const MASTER_ID = "5ff66a771a950d372db999b0"
 
 const handleMessage = (messageObj) => new Promise(async (res, rej) => {
     const numUsers = await UserModel.countDocuments()
     const numGear = await GearModel.countDocuments()
-    const n_sessions = await AnalyticsModel.countDocuments()
+    const n_sessions = await SessionModel.countDocuments()
     const packs = await PackModel.find({},{_id:0,sessionID:1,authorUserID:1}).lean()
     const entryNum = await AnalyticsModel.findByIdAndUpdate(MASTER_ID, {'$inc':{numEntries:1}}, {new: true}).lean()
     let numPacks = packs.length
